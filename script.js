7d78277de74a90113684c54e72cdfaab89c91387
Dominik Herold <dominik.herold@web.de>
2019-02-09 16:18:55 +0100

    Remove not needed language check

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// https://stackoverflow.com/a/29106129
-// todo add client language support
-function getFirstBrowserLanguage() {
-    var nav = window.navigator,
-    browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
-    i,
-    language;
-
-    // support for HTML 5.1 "navigator.languages"
-    if (Array.isArray(nav.languages)) {
-      for (i = 0; i < nav.languages.length; i++) {
-        language = nav.languages[i];
-        if (language && language.length) {
-          return language;
-        }
-      }
-    }
-
-    // support for other well known properties in browsers
-    for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
-      language = nav[browserLanguagePropertyKeys[i]];
-      if (language && language.length) {
-        return language;
-      }
-    }
-
-    return "en-US";
-  };
-  
 function update(data) {
 	var item;
 	var deaths = 0;
